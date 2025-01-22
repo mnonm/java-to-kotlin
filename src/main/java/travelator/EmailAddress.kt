aae7d6116aa0e44c0ed1699a6005a776ca763e45
songseongbin <ppsosqq@gmail.com>
2025-01-22 16:36:41 +0900

    [kotlin] EmailAddressTest data 변경자 추가
    - data 변경자 추가시 equals, hashCode, toString 구현 해줌

--- src/main/java/travelator/EmailAddress.kt
+++ src/main/java/travelator/EmailAddress.kt
@@ -2,21 +2,10 @@ package travelator
 
 import java.util.*
 
-class EmailAddress(
+data class EmailAddress(
     val localPart: String,
     val domain: String
 ) {
-    override fun equals(o: Any?): Boolean {
-        if (this === o) return true
-        if (o == null || javaClass != o.javaClass) return false
-        val that = o as EmailAddress
-        return localPart == that.localPart &&
-                domain == that.domain
-    }
-
-    override fun hashCode(): Int { // <5>
-        return Objects.hash(localPart, domain)
-    }
 
     override fun toString(): String {
         return "$localPart@$domain"
