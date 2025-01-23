3da6c2a29ba514a9ee4b7ce646d5b7ab265face5
songseongbin <ppsosqq@gmail.com>
2025-01-23 17:00:16 +0900

    [kotlin] UserPreferences, View - 빈에서 값으로 리팩토링

---
+++ src/main/java/travelator/mobile/Application.kt
@@ -0,0 +1,11 @@
+package travelator.mobile
+
+class Application(private var preferences: UserPreferences) {
+    fun showWelcome() {
+        WelcomeView(preferences).show()
+    }
+
+    fun editPreferences() {
+        preferences = PreferencesView().showModal(preferences)
+    }
+}
---
+++ src/main/java/travelator/mobile/UserPreferences.kt
@@ -0,0 +1,10 @@
+package travelator.mobile
+
+import java.util.*
+
+
+data class UserPreferences(
+    val greeting: String,
+    val locale: Locale,
+    val currency: Currency
+)
