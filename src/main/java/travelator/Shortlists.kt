b54067f739552a5e1194f9bfde279963c0ac7be7
songseongbin <ppsosqq@gmail.com>
2025-01-24 14:39:46 +0900

    [kotlin] 정적 메서드에서 최상위 함수로 - 최상위 메서드로 옮기기

--- src/main/java/travelator/Shortlists.kt
+++ src/main/java/travelator/Shortlists.kt
@@ -1,44 +1,34 @@
+@file:JvmName("Shortlists")
 package travelator
 
 import java.util.stream.Collectors
 import java.util.stream.Stream
 
-object Shortlists {
-    @JvmStatic
-    fun <T> sorted(
-        shortlist: List<T>,
-        ordering: Comparator<in T>?
-    ): List<T> {
-        return shortlist.stream()
-            .sorted(ordering)
-            .collect(Collectors.toUnmodifiableList<T>())
-    }
+fun <T> sorted(shortlist: List<T>, ordering: Comparator<in T>?): List<T> {
+    return shortlist.stream()
+        .sorted(ordering)
+        .collect(Collectors.toUnmodifiableList<T>())
+}
 
-    @JvmStatic
-    fun <T> removeItemAt(shortlist: List<T>, index: Int): List<T> {
-        return Stream.concat<T>(
-            shortlist.stream().limit(index.toLong()),
-            shortlist.stream().skip((index + 1).toLong())
-        ).collect(Collectors.toUnmodifiableList<T>())
-    }
+fun <T> removeItemAt(shortlist: List<T>, index: Int): List<T> {
+    return Stream.concat<T>(
+        shortlist.stream().limit(index.toLong()),
+        shortlist.stream().skip((index + 1).toLong())
+    ).collect(Collectors.toUnmodifiableList<T>())
+}
 
-    @JvmStatic
-    fun byRating(): Comparator<HasRating> {
-        return Comparator.comparingDouble(HasRating::rating).reversed()
-    }
+fun byRating(): Comparator<HasRating> {
+    return Comparator.comparingDouble(HasRating::rating).reversed()
+}
 
-    @JvmStatic
-    fun byPriceLowToHigh(): Comparator<HasPrice> {
-        return Comparator.comparing(HasPrice::price)
-    }
+fun byPriceLowToHigh(): Comparator<HasPrice> {
+    return Comparator.comparing(HasPrice::price)
+}
 
-    @JvmStatic
-    fun <T> byValue(): Comparator<T> where T : HasPrice?, T : HasRating? {
-        return Comparator.comparingDouble { t: T -> t!!.rating / t.price }.reversed()
-    }
+fun <T> byValue(): Comparator<T> where T : HasPrice?, T : HasRating? {
+    return Comparator.comparingDouble { t: T -> t!!.rating / t.price }.reversed()
+}
 
-    @JvmStatic
-    fun byRelevance(): Comparator<HasRelevance> {
-        return Comparator.comparingDouble(HasRelevance::relevance).reversed()
-    }
+fun byRelevance(): Comparator<HasRelevance> {
+    return Comparator.comparingDouble(HasRelevance::relevance).reversed()
 }
