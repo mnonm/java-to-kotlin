7cc21a8b309ad4189155b5293898d520a9db4679
songseongbin <ppsosqq@gmail.com>
2025-02-02 21:51:21 +0900

    [kotlin] 다중식 함수에서 단일식 함수로 - EmailAddress 리팩토링

--- src/main/java/travelator/EmailAddress.kt
+++ src/main/java/travelator/EmailAddress.kt
@@ -1,7 +1,5 @@
 package travelator
 
-import java.util.*
-
 data class EmailAddress(
     val localPart: String,
     val domain: String
@@ -13,13 +11,17 @@ data class EmailAddress(
 
     companion object {
         @JvmStatic
-        fun parse(value: String): EmailAddress {
-            val atIndex = value.lastIndexOf('@')
-            require(!(atIndex < 1 || atIndex == value.length - 1)) { "EmailAddress must be two parts separated by @" }
-            return EmailAddress(
-                value.substring(0, atIndex),
-                value.substring(atIndex + 1)
-            )
-        }
+        fun parse(value: String): EmailAddress =
+            value.splitAroundLast('@').let { (leftPart, rightPart) ->
+                EmailAddress(leftPart, rightPart)
+            }
+
+        private fun String.splitAroundLast(divider: Char): Pair<String, String> =
+            lastIndexOf(divider).let { index ->
+                require(index >= 1 && index != length - 1) {
+                    "string must be two non-empty parts separated by $divider"
+                }
+                substring(0, index) to substring(index + 1)
+            }
     }
 }
