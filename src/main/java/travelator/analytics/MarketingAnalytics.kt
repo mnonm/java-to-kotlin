42c26d0be6538802052c4d82b6b25ac2549f8244
songseongbin <ppsosqq@gmail.com>
2025-02-03 21:13:56 +0900

    [kotlin] 스트림에서 이터러블이나 시퀀스로 - typealias, 확장 프로퍼티 추가

--- src/main/java/travelator/analytics/MarketingAnalytics.kt
+++ src/main/java/travelator/analytics/MarketingAnalytics.kt
@@ -11,16 +11,16 @@ class MarketingAnalytics(
         return eventStore
             .queryAsSequence("type=CompletedBooking&timerange=$timeRange")
             .allEventsInSameInteractions()
-            .groupBy { event -> event["interactionId"] as String }
+            .groupBy(Event::interactionId)
             .values
             .averageBy { it.size }
     }
 
     fun Sequence<MutableMap<String, Any>>.allEventsInSameInteractions() =
         flatMap { event ->
-            val interactionId = event["interactionId"] as String
-            eventStore
-                .queryAsSequence("interactionId=$interactionId")
+            eventStore.queryAsSequence(
+                "interactionId=${event.interactionId}"
+            )
         }
 }
 
@@ -29,3 +29,8 @@ inline fun <T> Collection<T>.averageBy(selector: (T) -> Int): Double =
 
 fun EventStore.queryAsSequence(query: String) =
     this.queryAsStream(query).asSequence()
+
+typealias Event = Map<String, Any?>
+
+val Event.interactionId: String?
+    get() = this["interactionId"] as? String
