7bf2f3de62e65f9cb660af69d306e250505eafd2
songseongbin <ppsosqq@gmail.com>
2025-02-03 20:57:06 +0900

    [kotlin] 스트림에서 이터러블이나 시퀀스로 - Sequence 사용하도록 리팩토링

--- src/main/java/travelator/analytics/MarketingAnalytics.kt
+++ src/main/java/travelator/analytics/MarketingAnalytics.kt
@@ -1,6 +1,6 @@
 package travelator.analytics
 
-import java.util.stream.Collectors.groupingBy
+import kotlin.streams.asSequence
 
 class MarketingAnalytics(
     private val eventStore: EventStore
@@ -8,21 +8,22 @@ class MarketingAnalytics(
     fun averageNumberOfEventsPerCompletedBooking(
         timeRange: String
     ): Double {
-        val eventsForSuccessfulBookings =
-            eventStore
-                .queryAsStream("type=CompletedBooking&timerange=$timeRange")
-                .flatMap { event ->
-                    val interactionId = event["interactionId"] as String
-                    eventStore.queryAsStream("interactionId=$interactionId")
-                }
-        val bookingEventsByInteractionId =
-            eventsForSuccessfulBookings.collect(
-                groupingBy { event -> event["interactionId"] as String }
-            )
 
-        return bookingEventsByInteractionId.values.averageBy { it.size }
+        return eventStore
+            .queryAsSequence("type=CompletedBooking&timerange=$timeRange")
+            .flatMap { event ->
+                val interactionId = event["interactionId"] as String
+                eventStore
+                    .queryAsSequence("interactionId=$interactionId")
+            }
+            .groupBy { event -> event["interactionId"] as String }
+            .values
+            .averageBy { it.size }
     }
 
     inline fun <T> Collection<T>.averageBy(selector: (T) -> Int): Double =
         sumOf(selector) / size.toDouble()
+
+    fun EventStore.queryAsSequence(query: String) =
+        this.queryAsStream(query).asSequence()
 }
