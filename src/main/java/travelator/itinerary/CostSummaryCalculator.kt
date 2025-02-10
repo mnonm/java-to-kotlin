f6dbe1baa673c7d7377b79c00fc33beaa2cc0144
songseongbin <ppsosqq@gmail.com>
2025-02-10 19:00:53 +0900

    [kotlin] 누적시키는 객체에서 변환으로 - CostSummaryCalculator 리팩토링

--- src/main/java/travelator/itinerary/CostSummaryCalculator.kt
+++ src/main/java/travelator/itinerary/CostSummaryCalculator.kt
@@ -1,5 +1,6 @@
 package travelator.itinerary
 
+import travelator.money.CurrencyConversion
 import travelator.money.ExchangeRates
 import travelator.money.Money
 import java.util.*
@@ -8,25 +9,18 @@ class CostSummaryCalculator(
     private val userCurrency: Currency,
     private val exchangeRates: ExchangeRates
 ) {
-    private val currencyTotals = mutableMapOf<Currency, Money>()
-
-    fun addCost(cost: Money) {
-        currencyTotals.merge(cost.currency, cost, Money::add)
-    }
-
-    fun summarise(): CostSummary {
-        val lines = currencyTotals.values
+    fun summarise(costs: Iterable<Money>): CostSummary {
+        val currencyTotals: List<Money> = costs
+            .groupBy { it.currency }
+            .values
+            .map { moneys -> moneys.reduce(Money::add) }
+        val lines: List<CurrencyConversion> = currencyTotals
             .sortedBy { it.currency.currencyCode }
             .map { exchangeRates.convert(it, userCurrency) }
-
         val total = lines
             .map { it.toMoney }
             .fold(Money.of(0, userCurrency), Money::add)
-
         return CostSummary(lines, total)
     }
 
-    fun reset() {
-        currencyTotals.clear()
-    }
 }
