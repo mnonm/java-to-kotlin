02c419135e2d4bd7b2cb799e5709ed175a005796
songseongbin <ppsosqq@gmail.com>
2025-02-12 12:51:32 +0900

    [java] 캡슐화한 컬렉션에서 타입 별명으로 - UI

---
+++ src/main/java/travelator/UI.java
@@ -0,0 +1,45 @@
+package travelator;
+
+import travelator.itinerary.Journey;
+import travelator.itinerary.Route;
+import travelator.itinerary.RouteKt;
+
+import java.time.Duration;
+
+public class UI {
+
+    public void render(Iterable<Journey> route) {
+        for (var journey : route) {
+            render(journey);
+        }
+    }
+
+
+    public void render(Route route) {
+        for (int i = 0; i < RouteKt.size(route); i++) {
+            var journey = RouteKt.get(route, i);
+            render(journey);
+        }
+    }
+
+    public void renderWithHeader(Route route) {
+        renderHeader(
+            RouteKt.getDepartsFrom(route), // <1>
+            RouteKt.getArrivesAt(route),
+            RouteKt.getDuration(route)
+        );
+        for (int i = 0; i < RouteKt.size(route); i++) {
+            var journey = RouteKt.get(route, i);
+            render(journey);
+        }
+    }
+
+    private void render(Journey journey) {
+    }
+
+    private void renderHeader(
+        Location departsFrom,
+        Location arrivesAt,
+        Duration duration) {
+    }
+}
