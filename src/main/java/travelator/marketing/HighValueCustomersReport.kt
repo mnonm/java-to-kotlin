9f0972dec37cc342be0ceeee4c1b6a3cc26cb9da
songseongbin <ppsosqq@gmail.com>
2025-02-12 12:40:42 +0900

    [kotlin] 함수에서 확장 함수로 - 최상위 함수로 변경

--- src/main/java/travelator/marketing/HighValueCustomersReport.kt
+++ src/main/java/travelator/marketing/HighValueCustomersReport.kt
@@ -5,53 +5,45 @@ import java.io.Reader
 import java.io.Writer
 import java.util.*
 
-object HighValueCustomersReport {
-    @JvmStatic
-    @Throws(IOException::class)
-    fun generate(reader: Reader, writer: Writer) {
-        val valuableCustomers = reader
-            .readLines()
-            .toValuableCustomers()
-            .sortedBy(CustomerData::score)
+@Throws(IOException::class)
+fun generate(reader: Reader, writer: Writer) {
+    val valuableCustomers = reader
+        .readLines()
+        .toValuableCustomers()
+        .sortedBy(CustomerData::score)
 
-        writer.appendLine("ID\tName\tSpend")
-        for (customerData in valuableCustomers) {
-            writer.appendLine(customerData.outputLine)
-        }
-        writer.append(valuableCustomers.summarised())
+    writer.appendLine("ID\tName\tSpend")
+    for (customerData in valuableCustomers) {
+        writer.appendLine(customerData.outputLine)
+    }
+    writer.append(valuableCustomers.summarised())
+}
+
+private fun List<String>.toValuableCustomers() = withoutHeader()
+    .map { line -> line.toCustomerData() }
+    .filter { it.score >= 10 }
+
+private fun List<String>.withoutHeader() = drop(1)
+private fun List<CustomerData>.summarised(): String =
+    sumOf { it.spend }.let { total ->
+        "\tTOTAL\t${total.toMoneyString()}"
     }
 
-    private fun List<String>.toValuableCustomers() = withoutHeader()
-        .map { line -> line.toCustomerData() }
-        .filter { it.score >= 10 }
+internal fun String.toCustomerData(): CustomerData =
+    split("\t".toRegex()).dropLastWhile { it.isEmpty() }.let { parts ->
+        CustomerData(
+            id = parts[0],
+            givenName = parts[1],
+            familyName = parts[2],
+            score = parts[3].toInt(),
+            spend = if (parts.size == 4) 0.0 else parts[4].toDouble()
+        )
+    }
 
-    private fun List<String>.withoutHeader() = drop(1)
+private val CustomerData.outputLine: String
+    get() = "$id\t$marketingName\t${spend.toMoneyString()}"
 
-    private fun List<CustomerData>.summarised(): String =
-        sumOf { it.spend }.let { total ->
-            "\tTOTAL\t${total.toMoneyString()}"
-        }
-
-    @JvmStatic
-    internal fun String.toCustomerData(): CustomerData =
-        split("\t".toRegex()).dropLastWhile { it.isEmpty() }.let { parts ->
-            CustomerData(
-                id = parts[0],
-                givenName = parts[1],
-                familyName = parts[2],
-                score = parts[3].toInt(),
-                spend = if (parts.size == 4) 0.0 else parts[4].toDouble()
-            )
-        }
-
-
-    private val CustomerData.outputLine: String
-        get() = "$id\t$marketingName\t${spend.toMoneyString()}"
-
-    private fun Double.toMoneyString() = this.formattedAs("%#.2f")
-
-    private fun Any?.formattedAs(format: String) = String.format(format, this)
-
-    private val CustomerData.marketingName: String
-        get() = familyName.uppercase(Locale.getDefault()) + ", " + givenName
-}
+private fun Double.toMoneyString() = this.formattedAs("%#.2f")
+private fun Any?.formattedAs(format: String) = String.format(format, this)
+private val CustomerData.marketingName: String
+    get() = familyName.uppercase(Locale.getDefault()) + ", " + givenName
