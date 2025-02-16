48dd24e57c69c8dae4ce1912745064c63466bb6f
songseongbin <ppsosqq@gmail.com>
2025-02-16 20:55:28 +0900

    [kotlin] 목에서 맵으로 - RecommendationsTest 리팩토링

--- src/test/java/travelator/recommendations/RecommendationsTest.kt
+++ src/test/java/travelator/recommendations/RecommendationsTest.kt
@@ -1,151 +1,153 @@
-package travelator.recommendations;
+package travelator.recommendations
 
-import org.junit.jupiter.api.Test;
-import travelator.Id;
-import travelator.destinations.FeaturedDestination;
-import travelator.destinations.FeaturedDestinations;
-import travelator.domain.DistanceCalculator;
-import travelator.domain.Location;
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import travelator.Id
+import travelator.destinations.FeaturedDestination
+import travelator.domain.Location
+import java.util.Set
 
-import java.util.List;
-import java.util.Set;
+internal class RecommendationsTests {
 
-import static java.util.Collections.emptyList;
-import static java.util.Collections.emptySet;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+    private val featuredDestinations =
+        mutableMapOf<Location, List<FeaturedDestination>>()
+        .withDefault { emptyList() }
+    private val distanceInMetersBetween =
+        mutableMapOf<Pair<Location, Location>, Int>()
+            .withDefault { -1 }
 
-class RecommendationsTests {
+    private val recommendations = Recommendations(
+        featuredDestinations::getValue,
+        distanceInMetersBetween::getValue
+    )
+    private val paris = location("Paris")
+    private val louvre = featured("Louvre", "Rue de Rivoli")
+    private val eiffelTower = featured("Eiffel Tower", "Champ de Mars")
 
-    private final DistanceCalculator distanceCalculator =
-        mock(DistanceCalculator.class);
-    private final FeaturedDestinations featuredDestinations =
-        mock(FeaturedDestinations.class);
-    private final Recommendations recommendations = new Recommendations(
-        featuredDestinations::findCloseTo,
-        distanceCalculator::distanceInMetersBetween
-    );
-    private final Location paris = location("Paris");
-    private final FeaturedDestination louvre = featured("Louvre", "Rue de Rivoli");
-    private final FeaturedDestination eiffelTower = featured("Eiffel Tower", "Champ de Mars");
-
-    private final Location alton = location("Alton");
-    private final Location froyle = location("Froyle");
-    private final FeaturedDestination watercressLine = featured("Watercress Line", "Alton Station");
-    private final FeaturedDestination flowerFarm = featured("West End Flower Farm", froyle);
+    private val alton = location("Alton")
+    private val froyle = location("Froyle")
+    private val watercressLine = featured("Watercress Line", "Alton Station")
+    private val flowerFarm = featured("West End Flower Farm", froyle)
 
     @Test
-    public void returns_no_recommendations_when_no_locations() {
+    fun returns_no_recommendations_when_no_locations() {
         assertEquals(
-            emptyList(),
+            emptyList<Any>(),
             recommendations.recommendationsFor(emptySet())
-        );
+        )
     }
 
     @Test
-    public void returns_no_recommendations_when_no_featured() {
-        givenFeaturedDestinationsFor(paris, emptyList());
+    fun returns_no_recommendations_when_no_featured() {
+        givenFeaturedDestinationsFor(paris, emptyList())
         assertEquals(
-            emptyList(),
+            emptyList<Any>(),
             recommendations.recommendationsFor(Set.of(paris))
-        );
+        )
     }
 
     @Test
-    public void returns_recommendations_for_single_location() {
-        givenFeaturedDestinationsFor(paris,
-            List.of(
+    fun returns_recommendations_for_single_location() {
+        givenFeaturedDestinationsFor(
+            paris,
+            of(
                 eiffelTower,
                 louvre
-            ));
-        givenADistanceBetween(paris, eiffelTower, 5000);
-        givenADistanceBetween(paris, louvre, 1000);
+            )
+        )
+        givenADistanceFrom(paris, eiffelTower, 5000)
+        givenADistanceFrom(paris, louvre, 1000)
 
         assertEquals(
-            List.of(
-                new FeaturedDestinationSuggestion(paris, louvre, 1000),
-                new FeaturedDestinationSuggestion(paris, eiffelTower, 5000)
+            listOf(
+                FeaturedDestinationSuggestion(paris, louvre, 1000),
+                FeaturedDestinationSuggestion(paris, eiffelTower, 5000)
             ),
             recommendations.recommendationsFor(Set.of(paris))
-        );
+        )
     }
 
     @Test
-    public void returns_recommendations_for_multi_location() {
-        givenFeaturedDestinationsFor(paris,
-            List.of(
+    fun returns_recommendations_for_multi_location() {
+        givenFeaturedDestinationsFor(
+            paris,
+            of(
                 eiffelTower,
                 louvre
-            ));
-        givenADistanceBetween(paris, eiffelTower, 5000);
-        givenADistanceBetween(paris, louvre, 1000);
+            )
+        )
+        givenADistanceFrom(paris, eiffelTower, 5000)
+        givenADistanceFrom(paris, louvre, 1000)
 
-        givenFeaturedDestinationsFor(alton,
-            List.of(
+        givenFeaturedDestinationsFor(
+            alton,
+            of(
                 flowerFarm,
                 watercressLine
-            ));
-        givenADistanceBetween(alton, flowerFarm, 5300);
-        givenADistanceBetween(alton, watercressLine, 320);
+            )
+        )
+        givenADistanceFrom(alton, flowerFarm, 5300)
+        givenADistanceFrom(alton, watercressLine, 320)
 
         assertEquals(
-            List.of(
-                new FeaturedDestinationSuggestion(alton, watercressLine, 320),
-                new FeaturedDestinationSuggestion(paris, louvre, 1000),
-                new FeaturedDestinationSuggestion(paris, eiffelTower, 5000),
-                new FeaturedDestinationSuggestion(alton, flowerFarm, 5300)
+            listOf(
+                FeaturedDestinationSuggestion(alton, watercressLine, 320),
+                FeaturedDestinationSuggestion(paris, louvre, 1000),
+                FeaturedDestinationSuggestion(paris, eiffelTower, 5000),
+                FeaturedDestinationSuggestion(alton, flowerFarm, 5300)
             ),
             recommendations.recommendationsFor(Set.of(paris, alton))
-        );
+        )
     }
 
     @Test
-    public void deduplicates_using_smallest_distance() {
-        givenFeaturedDestinationsFor(alton,
-            List.of(
-                flowerFarm,
-                watercressLine
-            ));
-        givenADistanceBetween(alton, flowerFarm, 5300);
-        givenADistanceBetween(alton, watercressLine, 320);
+    fun deduplicates_using_smallest_distance() {
+        givenFeaturedDestinationsFor(alton, of(flowerFarm, watercressLine))
+        givenADistanceFrom(alton, to = flowerFarm, of = 5300)
+        givenADistanceFrom(alton, to = watercressLine, of = 320)
 
-        givenFeaturedDestinationsFor(froyle,
-            List.of(
-                flowerFarm,
-                watercressLine
-            ));
-        givenADistanceBetween(froyle, flowerFarm, 0);
-        givenADistanceBetween(froyle, watercressLine, 6300);
+        givenFeaturedDestinationsFor(froyle, of(flowerFarm, watercressLine))
+        givenADistanceFrom(froyle, to = flowerFarm, of = 0)
+        givenADistanceFrom(froyle, to = watercressLine, of = 6300)
 
         assertEquals(
-            List.of(
-                new FeaturedDestinationSuggestion(froyle, flowerFarm, 0),
-                new FeaturedDestinationSuggestion(alton, watercressLine, 320)
+            listOf(
+                FeaturedDestinationSuggestion(froyle, flowerFarm, 0),
+                FeaturedDestinationSuggestion(alton, watercressLine, 320)
             ),
-            recommendations.recommendationsFor(Set.of(alton, froyle))
-        );
+            recommendations.recommendationsFor(setOf(alton, froyle))
+        )
     }
 
-    private Location location(String name) {
-        return new Location(Id.of(name), name, name);
+    private fun location(name: String): Location {
+        return Location(Id.of(name), name, name)
     }
 
-    private FeaturedDestination featured(String name, String locationName) {
-        return featured(name, location(locationName));
+    private fun featured(name: String, locationName: String): FeaturedDestination {
+        return featured(name, location(locationName))
     }
 
-    private FeaturedDestination featured(String name, Location location) {
-        return new FeaturedDestination(name, location);
+    private fun featured(name: String, location: Location): FeaturedDestination {
+        return FeaturedDestination(name, location)
     }
 
-    private void givenFeaturedDestinationsFor(Location location, List<FeaturedDestination> result) {
-        when(featuredDestinations.findCloseTo(location))
-            .thenReturn(result);
+    private fun givenFeaturedDestinationsFor(
+        location: Location,
+        destinations: List<FeaturedDestination>
+    ) {
+        featuredDestinations[location] = destinations.toList()
     }
 
-    private void givenADistanceBetween(Location location, FeaturedDestination destination, int result) {
-        when(distanceCalculator.distanceInMetersBetween(location, destination.getLocation()))
-            .thenReturn(result);
+    private fun givenADistanceFrom(
+        from: Location,
+        to: FeaturedDestination,
+        of: Int
+    ) {
+        distanceInMetersBetween[from to to.location] = of
     }
 }
+
+private fun of(vararg destination: FeaturedDestination) = destination.toList()
+
+private fun <K1, K2, V> Map<Pair<K1, K2>, V>.getValue(k1: K1, k2: K2) =
+    getValue(k1 to k2)
