16c7ada65016e6b9e52e5bae398065c9a8c2eb2c
songseongbin <ppsosqq@gmail.com>
2025-03-05 22:58:13 +0900

    [kotlin] 인터페이스를 봉인된 클래스로 변환 - ItineraryItem 구현체에서 Description 분리

--- src/main/java/travelator/itinerary/ItineraryItem.kt
+++ src/main/java/travelator/itinerary/ItineraryItem.kt
@@ -2,14 +2,13 @@ package travelator.itinerary
 
 import travelator.Id
 import travelator.Location
-import travelator.geo.*
+import travelator.geo.Position
 import travelator.money.Money
 import java.time.Period
 import java.time.ZonedDateTime
 
 sealed class ItineraryItem {
     abstract val id: Id<ItineraryItem>
-    abstract val description: String
     abstract val costs: List<Money>
 }
 
@@ -23,8 +22,6 @@ data class Accommodation(
     val nights = Period.between(checkInFrom.toLocalDate(), checkOutBefore.toLocalDate()).days
     val totalPrice: Money = pricePerNight * nights
 
-    override val description
-        get() = "$nights nights at ${location.userReadableName}"
     override val costs
         get() = listOf(totalPrice)
 
@@ -35,10 +32,6 @@ data class Attraction(
     val location: Location,
     val notes: String
 ) : ItineraryItem() {
-    override val description
-        get() =
-            location.userReadableName
-
     override val costs
         get() =
             emptyList<Money>()
@@ -55,11 +48,6 @@ data class Journey(
     val price: Money,
     val path: List<Position> = listOf(departsFrom.position, arrivesAt.position),
 ) : ItineraryItem() {
-    override val description
-        get() = "${departsFrom.userReadableName} " +
-                "to ${arrivesAt.userReadableName} " +
-                "by ${travelMethod.userReadableName}"
-
     override val costs
         get() = listOf(price)
 
@@ -70,8 +58,6 @@ data class RestaurantBooking(
     val location: Location,
     val time: ZonedDateTime
 ) : ItineraryItem() {
-    override val description get() = location.userReadableName
-
     override val costs get() = emptyList<Money>()
 
 }
