66227a445a306aacd82cc679d29600931aa9be7e
songseongbin <ppsosqq@gmail.com>
2025-05-28 00:04:34 +0900

    [kotlin] 예외 던지기에서 값 반환으로 - CustomerRegistration test 변환

--- src/test/java/travelator/CustomerRegistrationTests.kt
+++ src/test/java/travelator/CustomerRegistrationTests.kt
@@ -1,59 +1,58 @@
-package travelator;
+package travelator
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import travelator.handlers.RegistrationData
+import java.util.*
 
-import java.util.Optional;
-import java.util.Set;
+class CustomerRegistrationTests {
+    var customers: InMemoryCustomers = InMemoryCustomers()
+    var excluded: Set<String> = setOf(
+        "cruella@hellhall.co.uk"
+    )
+    var registration: CustomerRegistration = CustomerRegistration(
+        customers
+    ) { registrationData: RegistrationData -> excluded.contains(registrationData.email) }
 
-import org.junit.jupiter.api.Test;
+    @Test
+    @Throws(DuplicateException::class, ExcludedException::class)
+    fun adds_a_customer_when_not_excluded() {
+        Assertions.assertEquals(Optional.empty<Any>(), customers.find("0"))
 
-import travelator.handlers.RegistrationData;
+        val added = registration.register(
+            RegistrationData("fred flintstone", "fred@bedrock.com")
+        )
+        Assertions.assertEquals(
+            Customer("0", "fred flintstone", "fred@bedrock.com"),
+            added
+        )
+        Assertions.assertEquals(added, customers.find("0").orElseThrow())
+    }
 
-public class CustomerRegistrationTests {
+    @Test
+    fun throws_DuplicateException_when_email_address_exists() {
+        customers.add(Customer("0", "fred flintstone", "fred@bedrock.com"))
+        Assertions.assertEquals(1, customers.size())
 
-	InMemoryCustomers customers = new InMemoryCustomers();
-	Set<String> excluded = Set.of(
-		"cruella@hellhall.co.uk"
-	);
-	CustomerRegistration registration = new CustomerRegistration(customers,
-		(registrationData) -> excluded.contains(registrationData.email)
-	);
+        Assertions.assertThrows(
+            DuplicateException::class.java
+        ) {
+            registration.register(
+                RegistrationData("another name", "fred@bedrock.com")
+            )
+        }
+        Assertions.assertEquals(1, customers.size())
+    }
 
-	@Test
-	public void adds_a_customer_when_not_excluded()
-		throws DuplicateException, ExcludedException {
-		assertEquals(Optional.empty(), customers.find("0"));
-
-		Customer added = registration.register(
-			new RegistrationData("fred flintstone", "fred@bedrock.com")
-		);
-		assertEquals(
-			new Customer("0", "fred flintstone", "fred@bedrock.com"),
-			added
-		);
-		assertEquals(added, customers.find("0").orElseThrow());
-	}
-
-	@Test
-	public void throws_DuplicateException_when_email_address_exists() {
-		customers.add(new Customer("0", "fred flintstone", "fred@bedrock.com"));
-		assertEquals(1, customers.size());
-
-		assertThrows(DuplicateException.class,
-			() -> registration.register(
-				new RegistrationData("another name", "fred@bedrock.com")
-			)
-		);
-		assertEquals(1, customers.size());
-	}
-
-	@Test
-	public void throws_ExcludedException_when_excluded() {
-		assertThrows(ExcludedException.class,
-			() -> registration.register(
-				new RegistrationData("cruella de vil", "cruella@hellhall.co.uk")
-			)
-		);
-		assertEquals(0, customers.size());
-	}
+    @Test
+    fun throws_ExcludedException_when_excluded() {
+        Assertions.assertThrows(
+            ExcludedException::class.java
+        ) {
+            registration.register(
+                RegistrationData("cruella de vil", "cruella@hellhall.co.uk")
+            )
+        }
+        Assertions.assertEquals(0, customers.size())
+    }
 }
