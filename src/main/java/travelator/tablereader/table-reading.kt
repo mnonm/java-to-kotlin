8429e1c19bd1095334491ef8b3dc0edf8a84274c
songseongbin <ppsosqq@gmail.com>
2025-07-24 21:04:08 +0900

    [kotlin] 클래스에서 함수로 - 단위테스트

--- src/main/java/travelator/tablereader/table-reading.kt
+++ src/main/java/travelator/tablereader/table-reading.kt
@@ -1,5 +1,13 @@
 package travelator.tablereader
 
-fun readTable(input: List<String>): List<Map<String, String>> {
-    TODO("Not yet implemented")
+fun readTable(lines: List<String>): List<Map<String, String>> {
+    return lines.map(::parseLine)
 }
+
+private fun parseLine(line: String): Map<String, String> {
+    val values = line.splitFields(",")
+    val keys = values.indices.map(Int::toString)
+    return keys.zip(values).toMap()
+}
+
+private fun String.splitFields(separators: String) = if (separators.isEmpty()) emptyList() else split(separators)
---
+++ src/test/java/travelator/tablereader/TableReaderTests.kt
@@ -0,0 +1,55 @@
+package travelator.tablereader
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class TableReaderTests {
+
+    @Test
+    fun `empty list returns empty list`() {
+        val input : List<String> = emptyList()
+        val expectedResult: List<Map<String, String>> = emptyList()
+        assertEquals(
+            expectedResult,
+            readTable(input)
+        )
+    }
+
+    @Test
+    fun `one line of input with default field names`() {
+        assertEquals(
+            listOf(
+                mapOf("0" to "field0", "1" to "field1")
+            ),
+            readTable(listOf(
+                "field0,field1"
+            ))
+        )
+    }
+
+    @Test
+    fun `empty line returns empty map`() {
+        assertEquals(
+            listOf(
+                emptyMap<String, String>()
+            ),
+            readTable(listOf(
+                ""
+            ))
+        )
+    }
+
+    @Test
+    fun `two lines of input with default field names`() {
+        assertEquals(
+            listOf(
+                mapOf("0" to "row0field0", "1" to "row0field1"),
+                mapOf("0" to "row1field0", "1" to "row1field1")
+            ),
+            readTable(listOf(
+                "row0field0,row0field1",
+                "row1field0,row1field1"
+            ))
+        )
+    }
+}
