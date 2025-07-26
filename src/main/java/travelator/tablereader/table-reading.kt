a43c622d869060d451ce95a6e09b6c09ee1523bf
songseongbin <ppsosqq@gmail.com>
2025-07-26 17:15:48 +0900

    [kotlin] 클래스에서 함수로 - TableReader 필드 구분자 리팩토링

--- src/main/java/travelator/tablereader/table-reading.kt
+++ src/main/java/travelator/tablereader/table-reading.kt
@@ -1,26 +1,50 @@
 package travelator.tablereader
 
 fun readTableWithHeader(
-    lines: List<String>
-): List<Map<String, String>> = when {
-    lines.isEmpty() -> emptyList()
-    else -> readTable(
-        lines.drop(1),
-        headerProviderFrom(lines.first())
-    )
-}
+    lines: List<String>,
+    splitter: (String) -> List<String> = splitOnComma
+): List<Map<String, String>> =
+    when {
+        lines.isEmpty() -> emptyList()
+        else -> readTable(
+            lines.drop(1),
+            headerProviderFrom(lines.first(), splitter),
+            splitter
+        )
+    }
 
-fun headerProviderFrom(header: String): (Int) -> String {
-    val headers = header.splitFields(",")
+fun headerProviderFrom(
+    header: String,
+    splitter: (String) -> List<String>
+): (Int) -> String {
+    val headers = splitter(header)
     return { index -> headers[index] }
 }
 
 fun readTable(
-    lines: List<String>, headerProvider: (Int) -> String = Int::toString
-): List<Map<String, String>> = lines.map { parseLine(it, headerProvider) }
+    lines: List<String>,
+    headerProvider: (Int) -> String = Int::toString,
+    splitter: (String) -> List<String> = splitOnComma
+): List<Map<String, String>> =
+    lines.map {
+        parseLine(it, headerProvider, splitter)
+    }
 
-private fun parseLine(line: String, headerProvider: (Int) -> String): Map<String, String> {
-    val values = line.splitFields(",")
+fun splitOn(
+    separators: String
+): (String) -> List<String> = { line ->
+    line.splitFields(separators)
+}
+
+val splitOnComma: (String) -> List<String> = splitOn(",")
+val splitOnTab: (String) -> List<String> = splitOn("\t")
+
+private fun parseLine(
+    line: String,
+    headerProvider: (Int) -> String,
+    splitter: (String) -> List<String>
+): Map<String, String> {
+    val values = splitter(line)
     val keys = values.indices.map(headerProvider)
     return keys.zip(values).toMap()
 }
